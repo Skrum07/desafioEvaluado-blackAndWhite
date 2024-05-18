5c4d54398ce0e4b5e0158baa2341f8bb9d27fc7f
Tomas Roa <tomas.roa@gmail.com>
2024-05-18 01:25:49 -0400

    se corrigen las rutas y se  corrige el archivo de css

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Carpeta publica
-app.use(express.static("assets"));
+
+app.use(express.urlencoded({extended: true}));
 
 app.use("/", routes);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import path from 'path';
+import Jimp from 'jimp';
+import { v4 as uuidv4 } from 'uuid';
 
 const __dirname = path.resolve();
 const routes = express.Router();
 
 
-
-
+routes.get('/styles', (req, res) => 
+    res.sendFile(__dirname + '/assets/css/style.css'),)
+ 
 routes.get('/', (req, res) => {
     res.sendFile( __dirname + '/view/index.html');
 });
 
+routes.post('/editar', async(req, res) => {
+
+    const imagen = req.body.URL
+    const id = uuidv4().slice(-5)
+    const nombreImagen = `${id}.jpg`
+    const img = await Jimp.read(imagen)
+    await img
+    .resize(350, Jimp.AUTO)
+    .greyscale()
+    .writeAsync(__dirname + `/assets/uploads/${nombreImagen}.jpg`)
+ 
+    res.sendFile(__dirname + `/assets/uploads/${nombreImagen}.jpg`)
+
+});
 export default routes;
